9f0380ecadeca4cd7a4bb32f4ffe9688b60aebfa
João Gabriel Do Vale Souza <145057648+joaogabriel343@users.noreply.github.com>
2025-04-28 14:15:06 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -66,7 +66,7 @@ const Index = () => {
             <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold text-white leading-tight mb-4">
                <span className="block">Promoção no Mês</span>
               <span className="bg-clip-text text-transparent bg-gradient-to-r from-sonhar-red-300 to-sonhar-red-600">
-                do dia das mães
+                das mães
               </span>
             </h1>
             <p className="text-lg md:text-xl text-white/80 mb-8 max-w-md">
